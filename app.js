b056504723c294b5a0108a466586960a00797d91
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-12-14 09:42:28 -0700

    client to server

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ app.set('views',path.join(__dirname,'views'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-    res.render('index',{users:[
-        {userName: 'Preston', email: 'pjevans@gmail.com'},
-        {userName: 'Preston', email: 'pjevans@gmail.com'},
-        {userName: 'Preston', email: 'pjevans@gmail.com'}
-    ],
-    date: new Date()})
+    res.render('index',{users:
+        [ 'brad','presto','test']})
+})
+app.get('/users/:username', (req,res) => {
+    res.end(`You clicked on: ${req.params.username}`)
 })
 
 app.listen(3000, () => {
