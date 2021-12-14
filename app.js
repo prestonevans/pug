ec647c619f540f2dde53bc582e5cf3f20840073a
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-12-14 10:13:45 -0700

    made create page

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.set('views',path.join(__dirname,'views'))
-app.set('view engine', 'pug')
+app.use(express.urlencoded({extended:false}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index',{users:
-        [ 'brad','presto','test']})
-})
-app.get('/users/:username', (req,res) => {
-    res.end(`You clicked on: ${req.params.username}`)
-})
+	res.render('index', {
+		users: [ 'brad', 'presto', 'test' ]
+	});
+});
+app.get('/users/:username', (req, res) => {
+	res.end(`You clicked on: ${req.params.username}`);
+});
+
+app.get('/form', (req, res) => {
+	res.render('form');
+});
+
+app.post('/create', (req, res) => {
+	const user = {
+		firstName: req.body.firstName,
+		lastName: req.body.lastName
+	};
+	res.end(`FirstName: ${user.firstName} LastName = ${user.lastName}`);
+});
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000')
-})
+	console.log('Listening on port 3000');
+});
