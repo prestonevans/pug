2b779d1b94d6ceb229fa5721a10130a1fd25f297
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-01 23:10:25 -0700

    almost finished

--- app.js
+++ app.js
@@ -1,42 +1,53 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs')
+const users = [ ]
 const app = express();
 
 app.use(express.urlencoded({extended:false}));
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-	res.render('index', {
-		users: [ 'brad', 'presto', 'test' ]
-	});
+	res.render('form')
 });
+
 app.get('/users/:username', (req, res) => {
 	res.end(`You clicked on: ${req.params.username}`);
 });
 
-app.get('/form', (req, res) => {
-	res.render('form');
+app.get('/users', (req, res) => {
+	res.render('index', {
+		users: users
+	})
 });
 
 app.post('/create', (req, res) => {
-	const timeStamp = new Date();
-	const timeStampString = timeStamp.toUTCString().replace(/\s/g,'')
-	const uid = `${req.body.name}${timeStampString}`
+	const uid = `${req.body.name}${Math.ceil(Math.random()*1000)}`
 	const user = {
 		name: req.body.name,
 		email: req.body.email,
 		age: req.body.age,
 		uid: uid
 	};
-	res.render('index', {
-		users: [ 'brad', 'presto', 'test' ]
-	});
-	res.end(`Name: ${user.name} Email = ${user.email} Age = ${user.age} UID = ${user.uid}`);
+	users.push(user)
+	res.redirect('/users')
+	fs.appendFile('./log.txt', `${JSON.stringify(user)}\n`, err => {
+		if (err) {
+			console.log(err)
+			return
+		}
+	})
+	res.end()
 });
+
+app.get('/delete/:uid', (req, res) => {
+	users.splice(users.findIndex(user => user.uid === req.params.uid),1)
+	res.redirect('/users')
+	res.end();
+})
 
 app.listen(3002, () => {
 	console.log('Listening on port 3000');
 });
+
